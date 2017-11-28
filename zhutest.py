c30b8d654b212df0f662d1635ee4fae0536969a6
jishu <1505007754@qq.com>
2017-11-28 15:05:29 +0800

    first

---
+++ zhutest.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+browser = webdriver.Firefox()
+first_url='http://mail.163.com/'
+browser.get(first_url)
+time.sleep(5)
+a = browser.find_element_by_xpath("//input[@name='email']")
+a.send_keys('15210242844')
+aaaaa
