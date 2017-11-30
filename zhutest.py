9208d1be03bc5c16cb45799122652f0e2519d2a3
jishu <1505007754>
2017-11-30 16:47:54 +0800

    变成因刚问

--- zhutest.py
+++ zhutest.py
@@ -4,10 +4,9 @@ import time
 browser = webdriver.Firefox()
 first_url='http://baidu.com'
 time.sleep(3)
-print u'diyige %s' % (first_url)
-browser.get(first_url)
+browser.find_element_by_id('kw').send_keys('seleniumm')
 time.sleep(3)
-seconr_url='http://v.baidu.com/'
-print u'dierge%s' %(seconr_url)
+browser.find_element_by_id('su').click()
 time.sleep(5)
+browser.close()
 
