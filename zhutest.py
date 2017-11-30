f86640179fcd7c5f87989a4533c6a4722313d7d2
jishu <1505007754>
2017-11-30 16:41:53 +0800

    变成中文

--- zhutest.py
+++ zhutest.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 import time
 browser = webdriver.Firefox()
 first_url='http://baidu.com'
-print u'进入第一个网页 %s' % (first_url)
+print u'diyige %s' % (first_url)
 browser.get(first_url)
 time.sleep(3)
 seconr_url='http://v.baidu.com/'
-print u'进入第二个视频网页 %s' %(seconr_url)
+print u'dierge%s' %(seconr_url)
 time.sleep(5)
-browser.quit()
+
