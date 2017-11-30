02441bb8f3360906c4f03b08f36b801fd26cbbc8
jishu <1505007754>
2017-11-30 16:49:57 +0800

    2121

--- zhutest.py
+++ zhutest.py
@@ -4,6 +4,8 @@ import time
 browser = webdriver.Firefox()
 first_url='http://baidu.com'
 time.sleep(3)
+browser.get(first_url)
+time.sleep(3)
 browser.find_element_by_id('kw').send_keys('seleniumm')
 time.sleep(3)
 browser.find_element_by_id('su').click()
