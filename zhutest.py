f58d90fbcf79bb058acb0d34a591745133949904
zhuyunchao <1505007754@qq.com>
2019-05-16 17:31:29 +0800

    hh

--- zhutest.py
+++ zhutest.py
@@ -12,3 +12,16 @@ browser.find_element_by_id('su').click()
 time.sleep(5)
 browser.close()
 
+#coding=utf-8
+from selenium import webdriver
+import time
+browser = webdriver.Firefox()
+first_url='http://baidu.com'
+time.sleep(3)
+browser.get(first_url)
+time.sleep(2)
+browser.find_element_by_id('kw').send_keys('seleniumm')
+time.sleep(3)
+browser.find_element_by_id('su').click()
+time.sleep(5)
+browser.close()
